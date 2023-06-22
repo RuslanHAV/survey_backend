12d1ec46fe7e538a4f2c796fd2a0b27698e96c12
Ruslan <devhavrylov@gmail.com>
2023-06-22 17:12:11 +0200

    change port

--- app.js
+++ app.js
@@ -39,4 +39,4 @@ app.get('/options/:id', (req, res) => {
     });
 });
 
-app.listen(3000, () => console.log('Listening on port 3000...'));
+app.listen(3001, () => console.log('Listening on port 3001...'));
